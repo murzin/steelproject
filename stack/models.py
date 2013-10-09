1ce352f28d0bdc633530a004a9fdf69dcbe5ce69
murzin <jvcmrz@gmail.com>
2013-10-10 01:09:14 +0300

    models created

--- stack/models.py
+++ stack/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+	caption = models.CharField(max_length=100)
+	text = models.TextField()
+	timestamp = models.DateTimeField()
+	author = models.ForeignKey(User)
+
+
+class Comment(models.Model):
+	text = models.TextField()
+	timestamp = models.DateTimeField()
+	author = models.ForeignKey(User)
+	question = models.ForeignKey(Question)
