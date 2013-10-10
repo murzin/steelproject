a8d077a3044be88ef793a5024f7e1a6e763f1050
murzin <jvcmrz@gmail.com>
2013-10-10 20:48:14 +0300

    comment form

--- stack/urls.py
+++ stack/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView, ListView
-from views import QuestionCreate, QuestionDetail
+from views import QuestionCreate, QuestionDetail, CommentCreate
 from models import Question
 
 urlpatterns = patterns('',
 	url(r'^$', ListView.as_view(template_name='index.html', model=Question), name='index'),
 	url(r'^question/add/$', QuestionCreate.as_view(), name='question_add'),
 	url(r'^question/(?P<pk>\d+)/$', QuestionDetail.as_view(), name='question_detail'),
+	url(r'^question/(?P<pk>\d+)/commentadd/$', CommentCreate.as_view(), name='comment_add'),
 )
--- stack/views.py
+++ stack/views.py
@@ -1,9 +1,9 @@
-from django.views.generic import DetailView
-from django.views.generic.edit import CreateView, ProcessFormView
+from django.views.generic import DetailView, TemplateView
+from django.views.generic.edit import CreateView, FormView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.shortcuts import get_object_or_404
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse
 from datetime import datetime
 from models import Question, Comment
 from forms import QuestionForm, CommentForm
@@ -24,8 +24,43 @@ class QuestionCreate(CreateView):
 		return super(QuestionCreate, self).dispatch(*args, **kwargs)
 
 
-class QuestionDetail(CreateView):
+#class QuestionDetail(CreateView):
+#	model = Comment
+#	form_class = CommentForm
+#	template_name = 'question_detail.html'
+#
+#	def form_valid(self, form):
+#		form.instance.author = self.request.user
+#		form.instance.timestamp = datetime.now()
+#		form.instance.question = get_object_or_404(Question, pk=self.kwargs['pk'])
+#		return super(QuestionDetail, self).form_valid(form)
+#
+#	def get_context_data(self, **kwargs):
+#		context = super(QuestionDetail, self).get_context_data(**kwargs)
+#		context['question'] = get_object_or_404(Question, pk=self.kwargs['pk'])
+#		return context
+#
+#	def get_success_url(self):
+#		return reverse('question_detail', kwargs={'pk': self.kwargs['pk']})
+#
+#	@method_decorator(login_required)
+#	def dispatch(self, *args, **kwargs):
+#		self.question = get_object_or_404(Question, pk=self.kwargs['pk'])
+#		return super(QuestionDetail, self).dispatch(*args, **kwargs)
 
+class QuestionDetail(DetailView):
+	model = Question
+	template_name = 'question_detail.html'
+	form_class = CommentForm
+
+	def get_context_data(self, **kwargs):
+		context = super(QuestionDetail, self).get_context_data(**kwargs)
+		context['form'] = CommentForm()
+		context['form_action'] = reverse('comment_add', kwargs={'pk': self.kwargs['pk']})
+		return context
+
+
+class CommentCreate(CreateView):
 	model = Comment
 	form_class = CommentForm
 	template_name = 'question_detail.html'
@@ -34,51 +69,12 @@ class QuestionDetail(CreateView):
 		form.instance.author = self.request.user
 		form.instance.timestamp = datetime.now()
 		form.instance.question = get_object_or_404(Question, pk=self.kwargs['pk'])
-		print 'form valid question pk = ' + self.kwargs['pk']
-		return super(QuestionDetail, self).form_valid(form)
-
-
-	def get_context_data(self, **kwargs):
-		context = super(QuestionDetail, self).get_context_data(**kwargs)
-		context['question'] = get_object_or_404(Question, pk=self.kwargs['pk'])
-		print 'context question pk = ' + self.kwargs['pk']
-		return context
+		return super(CommentCreate, self).form_valid(form)
 
 	def get_success_url(self):
-		return reverse_lazy('question_detail', kwargs={'pk': self.kwargs['pk']})
+		return reverse('question_detail', kwargs={'pk': self.kwargs['pk']})
 
-	@method_decorator(login_required)
+	@method_decorator(login_required(redirect_field_name=None))
 	def dispatch(self, *args, **kwargs):
-		self.question = get_object_or_404(Question, pk=self.kwargs['pk'])
-		return super(QuestionDetail, self).dispatch(*args, **kwargs)
+		return super(CommentCreate, self).dispatch(*args, **kwargs)
 
-
-
-
-
-
-#
-#
-#class QuestionDetail1(DetailView, ProcessFormView):
-#	model = Question
-#	template_name = 'question_detail.html'
-#	form_class = CommentForm
-#
-#	def get_context_data(self, **kwargs):
-#		context = super(QuestionDetail, self).get_context_data(**kwargs)
-#		context['form'] = CommentForm()
-#		return context
-
-
-
-
-
-
-
-#class TakeQuestion(FormView):
-#	template_name = 'take_question.html'
-#	form_class = QuestionForm
-#	success_url = '/'
-#
-#	def form_valid(self, form):
-#		return super(TakeQuestion, self).form_valid(form)
